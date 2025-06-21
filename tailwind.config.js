b90e256a24343bd6ebab29b9fae16bc2f9307e51
try-again-later <aa.popok@yandex.ru>
2025-06-21 18:57:32 +0300

    [Livewire v2 -> v3] Upgrade frontend dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,9 @@
 const colors = require('tailwindcss/colors');
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-    ],
-    theme: {
-        extend: {
-            colors: {
-                'status-open': colors.gray[300],
-                'status-in-progress': colors.yellow[400],
-                'status-implemented': colors.emerald[400],
-                'status-considering': colors.indigo[400],
-                'status-closed': colors.red[400],
-
-                'status-hover-open': colors.gray[200],
-                'status-hover-in-progress': colors.yellow[300],
-                'status-hover-implemented': colors.emerald[300],
-                'status-hover-considering': colors.indigo[300],
-                'status-hover-closed': colors.red[300],
-
-                'status-text-open': colors.gray[700],
-                'status-text-in-progress': colors.white,
-                'status-text-implemented': colors.white,
-                'status-text-considering': colors.white,
-                'status-text-closed': colors.white,
-            },
-        }
-    },
-    plugins: [
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/line-clamp'),
     ],
 };
